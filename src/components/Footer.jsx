22fad1c4d572c2bc098e050bb81031f7536b6b63
maruvivas <mariavivasdecanio@gmail.com>
2022-11-29 20:19:25 -0300

    drawing footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <Footer>
+            <div className="cards"></div>
+            <p>Todos los derechos reservados</p>
+            <p>Contácatanos</p>
+        </Footer>
+    )
+}
+
+export default Footer
