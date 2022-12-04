ad79a6e5d90d7e9561e10e8b392597dfc654a632
Mario Quevedo <mb-quevedo@hotmail.com>
2022-12-04 02:06:23 -0300

    I add content for the detail view

--- src/views/Guitar.jsx
+++ src/views/Guitar.jsx
@@ -1,9 +1,17 @@
-const Guitar = ()=>{
-
-    return(
+const Guitar = () => {
+    return (
         <main>
             <div className="detail-guitar">
-                <h1>Detalle</h1>
+                <h1>Detalle de Producto</h1>
+                <h2>Fender Jaguar® 60's American Original</h2>
+                <img src="https://www.fender.cl/media/catalog/product/cache/1/image/800x800/9df78eab33525d08d6e5fb8d27136e95/g/e/ge468_0110160800v1.jpg" alt="fender jaguar" />
+                <h4> Cápsulas: S/S
+                    <br/>
+                    Diapasón: Palo de Rosa Rosewood
+                    <br/>
+                    Color: 3-color Sunburst
+                </h4>
+                <h4>Nunca descansamos en nuestra misión de mejorar la vida de los músicos y creamos la guitarra Jaguar de escala corta en 1962. Originalmente lanzada como nuestra guitarra eléctrica de más alta calidad y con más detalles, inspiró a los héroes del underground musical y con su ritmo a los del surf, punk y guitarristas alternativos. La Jaguar American Original de los años 60 tiene ese sonido flexible y una apariencia furtiva, ligeramente retocada para una sensación más moderna.</h4>
             </div>
         </main>
     )
