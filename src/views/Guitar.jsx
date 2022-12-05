1029b7cc6df532d9a2fb6c74c0e76eee138a9f41
Mario Quevedo <mb-quevedo@hotmail.com>
2022-12-05 16:38:30 -0300

    add view see more

--- src/views/Guitar.jsx
+++ src/views/Guitar.jsx
@@ -1,18 +1,37 @@
+import { useParams } from "react-router-dom"
+import { useContext, useState, useEffect } from "react"
+
+import Context from "../context/context"
+
 const Guitar = () => {
+    const { id } = useParams()
+    const { guitars } = useContext(Context)
+
+    const [ guitar, setGuitar ] = useState({})
+
+    useEffect(()=> {
+        const guitar = guitars.filter((item)=> item.id === id)
+        setGuitar(guitar[0])
+
+    }, [])
+
     return (
         <main>
             <div className="detail-guitar">
-                <h1>Detalle de Producto</h1>
-                <h2>Fender Jaguar® 60's American Original</h2>
-                <img src="https://www.fender.cl/media/catalog/product/cache/1/image/800x800/9df78eab33525d08d6e5fb8d27136e95/g/e/ge468_0110160800v1.jpg" alt="fender jaguar" />
-                <h4> Cápsulas: S/S
-                    <br/>
-                    Diapasón: Palo de Rosa Rosewood
-                    <br/>
-                    Color: 3-color Sunburst
-                </h4>
-                <h4>Nunca descansamos en nuestra misión de mejorar la vida de los músicos y creamos la guitarra Jaguar de escala corta en 1962. Originalmente lanzada como nuestra guitarra eléctrica de más alta calidad y con más detalles, inspiró a los héroes del underground musical y con su ritmo a los del surf, punk y guitarristas alternativos. La Jaguar American Original de los años 60 tiene ese sonido flexible y una apariencia furtiva, ligeramente retocada para una sensación más moderna.</h4>
-            </div>
+                <section className="image-detail" style={{backgroundImage: `url(${guitar.img})`}}>
+                </section>
+                <article className="content">
+                    <h4>{ guitar.name }</h4>
+                    <h5 className="desc">{ guitar.desc }</h5>
+                    <h5> { guitar.specifications } </h5>
+
+                    <div className="price-row">
+                        <h3>Precio: $ { guitar.price }</h3>
+
+                        <button className="btn btn-primary">Añadir</button>
+                    </div>
+                </article>
+            </div> 
         </main>
     )
 }
