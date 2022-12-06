737e88e9de4cf74de975086ea1392a9ee878a980
maruvivas <mariavivasdecanio@gmail.com>
2022-12-06 08:14:23 -0300

    styles improvements

--- src/views/Profile.jsx
+++ src/views/Profile.jsx
@@ -4,14 +4,14 @@ const Profile = () => {
     return (
         <div className="main-profile">
             <div className="profile">
-                <h2>Bienvenida Ma Eugenia!</h2>
+                <h2>Bienvenido Cosme</h2>
                 <div className="profile-container">
                     <div className="profile-info">
                         <img className="avatar" src="https://images.unsplash.com/photo-1620330388384-31eddaadf267?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80image.png"></img>
                         <div className="info">
-                            <p>Mi puntuación</p>
+                            <p>Mi puntuación: ⭐⭐⭐</p>
                             <p>Medio de pago: xxxx5533</p>
-                            <p>Dirección: Las Hualtatas 2342</p>
+                            <p>Dirección: Av. Siempreviva 123</p>
                         </div>
                     </div>
                     <div className="profile-links">
@@ -24,7 +24,7 @@ const Profile = () => {
                             
                         </div>
                         <div className="link my-cart">
-                            <i className="fa-solid fa-cart-shopping"></i>
+                            <Link to="/carrito"><i className="fa-solid fa-cart-shopping"></i></Link>
                             
                         </div>
                     </div>
@@ -32,7 +32,7 @@ const Profile = () => {
                 </div>
             </div>
             <aside className="additional-products">
-                <h3>Ofertas imperdibles:</h3>
+                <h3>Ofertas para ti:</h3>
                 <div className="aside-product">
                     <img src="https://http2.mlstatic.com/D_NQ_NP_785862-MLC47757592647_102021-O.jpg"></img>
                     <p>Juego de uñetas</p>
