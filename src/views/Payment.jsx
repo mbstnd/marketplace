f8966b0a99f5180d6541fd224eefc0c9fc7eba8f
Mario Quevedo <mb-quevedo@hotmail.com>
2022-12-06 16:38:48 -0300

    modifico font size de botones del home y de la vista de detalle de producto

--- src/views/Payment.jsx
+++ src/views/Payment.jsx
@@ -3,10 +3,8 @@ const Payment = ()=> {
         <main className="payment">
             <i className="fa-solid fa-credit-card"></i>
             <h1>Redirigiendo a la pasarela de pagos...</h1>
-
         </main>
     )
-
 }
 
 export default Payment
