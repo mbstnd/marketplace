a75661b30d60d70842a89c140d74d3767f2ede9f
maruvivas <mariavivasdecanio@gmail.com>
2022-12-13 11:05:28 -0300

    Styles to make gallery responsive

--- src/views/Gallery.jsx
+++ src/views/Gallery.jsx
@@ -7,31 +7,29 @@ const Gallery = ({searchedItem, onSearch, onSort, sortOrder}) => {
 
     return (
         <div className="container-gallery">
-        <aside className="filter-aside">
-           
-                <div className="filtro" >
-                <input type="text" placeholder=" Filtra" value={searchedItem} onChange={e => onSearch(e.target.value)} ></input>
-                <button className="btn-filter">Filtra</button>
-                </div>
-                <div className="filtro"  >
-                <select value ={sortOrder} onChange={e => onSort(e.target.value)} >
-                    <option value="0">Ordena</option>
-                    <option value="asc">Menor precio</option>
-                    <option value="desc">Mayor precio</option>
-                </select>
-                <button className="btn-filter">Borrar filtros</button>
-                </div>
-             
-        </aside>
-        <div className="container row row-cols-2 row-cols-md-2  m-4 g-4 main-gallery">
-            {
-                guitars.map((guitar) => {
-                    return (
-                        <Card guitar={ guitar } key={guitar.id}></Card>
-                    )
-                })
-            }
-        </div>
+            <aside className="filter-aside">
+            
+                    <div className="filtro" >
+                    <input type="text" placeholder=" Palabra clave" value={searchedItem} onChange={e => onSearch(e.target.value)} ></input>
+                    </div>
+                    <div className="filtro"  >
+                    <select value ={sortOrder} onChange={e => onSort(e.target.value)} >
+                        <option value="0">Ordena</option>
+                        <option value="asc">Menor precio</option>
+                        <option value="desc">Mayor precio</option>
+                    </select>
+                    </div>
+                
+            </aside>
+            <div className="container  row  m-4 g-4 main-gallery">
+                {
+                    guitars.map((guitar) => {
+                        return (
+                            <Card guitar={ guitar } key={guitar.id}></Card>
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 }
